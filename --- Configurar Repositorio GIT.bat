f56312a1ab1b7e4ac4296b5e6a716aeba0aa03c7
Mauro <mauro.masciar@gmail.com>
2026-07-13 22:52:22 -0300

    1

---
+++ --- Configurar Repositorio GIT.bat
@@ -0,0 +1,8 @@
+echo "# SDL" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MauroMasciar/DYWTPN2.git
+git push -u origin main
+pause
